316381d4ad54da162b91e7cb3552cf37fcdcef11
AHJ <savior0319@naver.com>
2018-05-18 14:25:39 +0900

    -

--- src/member/controller/Login.java
+++ src/member/controller/Login.java
@@ -1,3 +1,30 @@
+/*
+ 
+DROP TABLE MEMBER;
+
+CREATE TABLE MEMBER(
+  MEMBER_ID VARCHAR2(20) PRIMARY KEY,
+  MEMBER_PWD VARCHAR2(20) NOT NULL,
+  MEMBER_NAME VARCHAR2(20) NOT NULL,
+  MEMBER_AGE NUMBER,
+  MEMBER_ADDR VARCHAR2(20)
+);
+
+INSERT INTO MEMBER VALUES
+('admin', 'pwd1', '관리자', 20, '인천');
+
+INSERT INTO MEMBER VALUES
+('user11', 'pwd112', '이름1', 30, '서울');
+
+INSERT INTO MEMBER VALUES
+('user22', 'pwd119', '이름2', 40, '부산');
+
+COMMIT;
+
+SELECT * FROM MEMBER;
+ 
+ */
+
 package member.controller;
 
 import java.io.IOException;
@@ -28,39 +55,50 @@ public class Login extends HttpServlet {
 		MemberVO m = new MemberService().selectMember(userId, userPwd);
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		if (m == null) {
+
+		if (m == null) { // 로그인 실패
 			out.println("<html>");
 			out.println("<head>");
 			out.println("<title>");
 			out.println("결과");
 			out.println("</title>");
 			out.println("</head>");
+			out.println("<style>");
+			out.println("h1{color : red;}");
+			out.println("</style>");
 			out.println("<body>");
 			out.println("<center>");
 			out.println("<h1>");
-			out.println("회원정보");
+			out.println("로그인 실패");
 			out.println("</h1>");
 			out.print("<hr>");
 			out.println("<h3>");
-			out.println("회원 정보를 잘못 입력했습니다");
+			out.println("아이디 또는 비밀번호를 확인하세요");
 			out.println("</h3>");
 			out.print("<hr>");
 			out.println("</center>");
 			out.println("</body>");
 			out.println("</html>");
-		} else {
+		} else { // 로그인 성공
 			out.println("<html>");
 			out.println("<head>");
 			out.println("<title>");
 			out.println("결과");
 			out.println("</title>");
 			out.println("</head>");
+			out.println("<style>");
+			out.println("h1{color : blue;} a{text-decoration:none;}");
+			out.println("</style>");
 			out.println("<body>");
 			out.println("<center>");
 			out.println("<h1>");
-			out.println("회원정보");
+			out.println(m.getMemberName() + "님 환영합니다");
 			out.println("</h1>");
-			out.print("<hr>");
+			out.println("<hr>");
+			out.println("<h2>");
+			out.println("회원정보");
+			out.println("</h2>");
+			out.println("<hr>");
 			out.println("<h3>");
 			out.println("아이디 : " + m.getMemberID());
 			out.println("<br>");
@@ -73,6 +111,9 @@ public class Login extends HttpServlet {
 			out.println("주소 : " + m.getMemberAddr());
 			out.println("</h3>");
 			out.print("<hr>");
+			out.println("<a href=myInfo>마이페이지</a>");
+			out.println("<br>");
+			out.println("<a href=logout>로그아웃</a>");
 			out.println("</center>");
 			out.println("</body>");
 			out.println("</html>");
